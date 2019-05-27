5bcff9baeb0fd10fb9f4ef2b28f4b46028a5cc60
huy <tranquanghuy097@gmail.com>
2019-05-27 09:26:22 +0700

    Add pagination

--- model/newsModel.js
+++ model/newsModel.js
@@ -1,15 +1,19 @@
 var db = require('../utilities/utilities')
 
 module.exports = {
-    loadbySub: subcategory => {
-        return db.load(`select * from news where subcategory = ${subcategory}`);
-    },
-
     loadbyPage: (limit, offset) => {
         return db.load(`select * from news limit ${limit} offset ${offset}`)
     },
 
     loadbyBottom: (limit, offset) => {
         return db.load(`select * from news order by id desc limit ${limit} offset ${offset}`)
-    }
+    },
+
+    loadbyCat: (category, limit, offset) => {
+        return db.load(`select * from news where subcategory = '${category}' limit ${limit} offset ${offset}`)
+    },
+
+    countCat: (category) => {
+        return db.load(`select count(*) as total from news where subcategory = '${category}'`)
+    },
 }
--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,35 @@ index.get('/', (req, res, next) => {
 
 })
 
+index.get('/:cat', function(req, res, next) {
+    var cat = req.params.cat;
+    var page = req.query.page || 1;
+    var limit = 10;
+
+    if(page < 1)
+        page = 1;
+    var offset = (page - 1) * limit;
+
+    Promise.all([news.loadbyCat(cat, limit, offset),
+                news.countCat(cat)])
+        .then(([rows, count_rows]) => {
+            var total = count_rows[0].total;
+            var nPage = Math.floor(total / limit);
+            if(total % limit > 0)
+                nPage++;
+            var pages = [];
+            for(i = 1; i <= nPage; i++){
+                var obj = {value: i, active: i === +page};
+                pages.push(obj);
+            }
+            console.log(pages)
+            res.render('category', {
+                category: cat,
+                news: rows,
+                pages,
+            })
+        })
+        .catch(next)
+})
+
 module.exports = index;
